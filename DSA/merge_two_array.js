ad163f7ec74c4e065c40675020fa67af39079433
Sudhanshu <sudhanshu.sharma@psiborg.in>
2024-03-15 09:36:13 +0530

    Old

--- DSA/merge_two_array.js
+++ DSA/merge_two_array.js
@@ -1,10 +1,12 @@
 let arr4 = [11, 16, 17, 14];
-let arr5 = [15, 12, 13, 18];
+let arr5 = [15, 12, 13, 18, 30];
 let newArr = [];
 
 for (let i = 0; i < arr4.length; i++) {
   newArr.push(arr4[i]);
-  newArr.push(arr5[i]);
+}
+for (let j = 0; j < arr5.length; j++) {
+  newArr.push(arr5[j]);
 }
 
 console.log(newArr);
